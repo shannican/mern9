df2ef8d9a42594b2956c07cd682b6dc98f16df69
shannican <shannimura8683@gmail.com>
2024-07-12 09:20:35 +0530

    made story sample

--- script.js
+++ script.js
@@ -7,17 +7,34 @@ var arr = [
         {
                 username:"Beedi vaale uncle",
                 dp:"https://images.unsplash.com/photo-1493106819501-66d381c466f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                story:""
+                story:"https://images.unsplash.com/photo-1720649718712-dff5514d5534?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
                 username:"Sweeti kumari",
                 dp:"https://images.unsplash.com/photo-1495462911434-be47104d70fa?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                story:""
+                story:"https://images.unsplash.com/photo-1720692739658-ee952b1aebb1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
                 username:"Lovely Kumari",
                 dp:"https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                story:""
-        }
+                story:"https://images.unsplash.com/photo-1720462813863-cf94aef89b38?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        },
+        {
+                username:"Babita kumari",
+                dp:"https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                story:"https://images.unsplash.com/photo-1717701315033-492b4c8b0d23?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D"
+        },
         
 ]
+
+var clutter = ""
+
+arr.forEach(function(elem,idx){
+        clutter = clutter + `<div class="story">
+                    <img id=${idx} src="${elem.dp}" alt="">
+                </div>`
+})
+
+var storiyan = document.querySelector("#storiyan")
+
+storiyan.innerHTML = clutter
